5195463e56e179261c5bef894443ccdb8ed3b7ac
Param Ri <paramri@gatech.edu>
2018-10-20 01:43:19 -0400

    begin button leads to SelectActivity

--- app/src/main/java/optimizedtravelassistant/hackventure/com/optimizedtravelassistant/Welcome.java
+++ app/src/main/java/optimizedtravelassistant/hackventure/com/optimizedtravelassistant/Welcome.java
@@ -1,13 +1,29 @@
 package optimizedtravelassistant.hackventure.com.optimizedtravelassistant;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
+
+    private Button beginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        beginButton = (Button) findViewById(R.id.beginButton);
+
+        beginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Welcome.this, SelectionActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }

73e887594fc1b5b8cc10f2cbc986d4337ae10762
MindMaster31415 <sumedhgarimella@gmail.com>
2018-10-21 03:11:11 -0400

    sending http requests

--- app/src/main/java/optimizedtravelassistant/hackventure/com/optimizedtravelassistant/SelectionActivity.java
+++ app/src/main/java/optimizedtravelassistant/hackventure/com/optimizedtravelassistant/SelectionActivity.java
@@ -3,6 +3,7 @@ package optimizedtravelassistant.hackventure.com.optimizedtravelassistant;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RadioGroup;
 import android.widget.RadioButton;
@@ -13,12 +14,18 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Button;
 import android.widget.SeekBar;
 import org.json.*;
+import com.android.volley.*;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 public class SelectionActivity extends AppCompatActivity {
+
 
     private Button prefButton;
     private Button settingsButton;
     private RadioGroup radioGroup;
+
+    private String URL = "https://hackventure---ha-1539747396872.appspot.com/sorter";
 
     private int thrill = 0;
     private int active = 0;
@@ -67,6 +74,45 @@ public class SelectionActivity extends AppCompatActivity {
                         sort_value = 0;
                 }
                  */
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                double[] coords = {33,-84};
+                String coordinates = coords[0] + "," + coords[1];
+
+                JSONObject packet = new JSONObject();
+
+                try {
+                    packet.put("algorithm_num", (int) 0);
+                    packet.put("gps_coordinates", coordinates);
+                    packet.put("user_preference", preferences);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                        (Request.Method.POST, URL, packet, new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.i("response", response.toString());
+                                JSONObject result = response;
+                            }
+                        }, new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // TODO: Handle error
+
+                            }
+                        });
+
+                queue.add(jsonObjectRequest);
+                Intent i = new Intent(SelectionActivity.this, AttractionMapActivity.class);
+                //i.putExtra("result_json", result);
+                startActivity(i);
+
+
+
+                /**
                 JSONObject packet = new JSONObject();
                 try {
                     packet.put("preferences", preferences);
@@ -74,11 +120,10 @@ public class SelectionActivity extends AppCompatActivity {
                     e.printStackTrace();
 
                 }
+*/
 
 
 
-                Intent i = new Intent(SelectionActivity.this, AttractionMapActivity.class);
-                startActivity(i);
                 /**
                  Intent i = new Intent(Welcome.this, SelectionActivity.class);
                  startActivity(i);
